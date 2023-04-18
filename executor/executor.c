7f37272796f2404168a2e0b753f6085fff98428c
Tosh-Sharma <toshkumar.s@somaiya.edu>
2023-04-18 18:05:10 +0200

    Updated code to allow single command execution without pipes and added some cleanup code as well.

--- executor/executor.c
+++ executor/executor.c
@@ -6,7 +6,7 @@
 /*   By: toshsharma <toshsharma@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/06 22:54:48 by toshsharma        #+#    #+#             */
-/*   Updated: 2023/04/18 16:09:09 by toshsharma       ###   ########.fr       */
+/*   Updated: 2023/04/18 18:01:15 by toshsharma       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,8 +26,6 @@ void	pipe_commands(t_shell *shell, char *command)
 	if (id == 0)
 	{
 		io_redirection(shell, 1, fd[1]);
-		// dup2(fd[1], STDOUT_FILENO);
-		// dup2(shell->temp_fd, STDIN_FILENO);
 		close(shell->temp_fd);
 		close(fd[0]);
 		close(fd[1]);
@@ -51,7 +49,6 @@ void	multipipe_last(t_shell *shell, char *command)
 	if (id == 0)
 	{
 		io_redirection(shell, 1, -1);
-		// dup2(shell->temp_fd, STDIN_FILENO);
 		close(shell->temp_fd);
 		execute_process(shell, command);
 	}
@@ -69,13 +66,22 @@ void	execute_commands(t_shell *shell, char **splitted_commands, int count)
 
 	i = -1;
 	shell->temp_fd = dup(STDIN_FILENO);
-	while (++i < (count - 1))
+	if (count == 1)
+		single_command_execution(shell, splitted_commands);
+	else
 	{
+		while (++i < (count - 1))
+		{
+			shell->split_com = ft_split(splitted_commands[i], ' ');
+			set_io_redirection_flags(shell);
+			pipe_commands(shell, splitted_commands[i]);
+			free_strings(shell->split_com);
+		}
 		shell->split_com = ft_split(splitted_commands[i], ' ');
 		set_io_redirection_flags(shell);
-		pipe_commands(shell, splitted_commands[i]);
+		multipipe_last(shell, splitted_commands[i]);
+		free_strings(shell->split_com);
+		if (access("input.txt", F_OK) == 0)
+			unlink("input.txt");
 	}
-	shell->split_com = ft_split(splitted_commands[i], ' ');
-	set_io_redirection_flags(shell);
-	multipipe_last(shell, splitted_commands[i]);
 }
